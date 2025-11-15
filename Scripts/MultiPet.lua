dffcbca26d02a43dbd8db7a414600124d73e5de4
1500665289 <152504086+1500665289@users.noreply.github.com>
2025-11-15 18:55:56 +0800

    Initial commit

---
+++ Scripts/MultiPet.lua
@@ -0,0 +1,23 @@
+﻿print("Loading MultiPet mod")
+local mod = GameMain:GetMod("MultiPet");
+function mod:OnBeforeInit()
+  print("[MultiPet] OnBeforeInit")
+  xlua.private_accessible(CS.XLua.LuaEnv)
+  xlua.private_accessible(CS.XLua.ObjectTranslator)
+  local thisData = CS.ModsMgr.Instance:FindMod("MultiPet", nil, true)
+  local thisPath = thisData.Path
+  print("[MultiPet] path: " .. thisPath)
+  local mllFile = CS.System.IO.Path.Combine(thisPath, "MultiPet.dll")
+  local asm = CS.System.Reflection.Assembly.LoadFrom(mllFile)
+  CS.XiaWorld.LuaMgr.Instance.Env.translator.assemblies:Add(asm)
+  -- CS.MultiPet.MultiPet.Patch()
+end
+
+function mod:OnLoad()
+  print("[MultiPet] OnLoad")
+end
+
+function mod:OnLeave()
+  print("[MultiPet] OnLeave")
+  CS.MultiPet.MultiPetPatcher.Unpatch()
+end
